962cac5fb9b97104501c9ec11314b379fa245cd4
tariq2518 <tariq.hussain2518@gmail.com>
2023-06-04 16:22:41 +0500

    Setting up dependencies for the Anime Heroes app

---
+++ app/src/main/java/com/tariq/animeheroes/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.tariq.animeheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tariq.animeheroes.ui.theme.AnimeHeroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimeHeroesTheme {
+
+            }
+        }
+    }
+}
+
